01578693a8fec7343b97f289a47a11acb370fa2e
Trevor Slocum <tslocum@gmail.com>
2015-10-22 14:20:48 -0700

    Add widgets to open source release

--- MeditationAssistantWear/src/main/java/sh/ftp/rocketninelabs/meditationassistant/WearEditDurationActivity.java
+++ MeditationAssistantWear/src/main/java/sh/ftp/rocketninelabs/meditationassistant/WearEditDurationActivity.java
@@ -1,13 +1,8 @@
 package sh.ftp.rocketninelabs.meditationassistant;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.wearable.view.DismissOverlayView;
 import android.support.wearable.view.WatchViewStub;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TimePicker;
 
